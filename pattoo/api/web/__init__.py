9bc70b534e85fb71a71cdf79daddf5aae5864a99
Peter Harrison <peter@colovore.com>
2019-11-12 18:34:53 -0800

    Supports PattooShared v0.0.28

--- pattoo/api/web/__init__.py
+++ pattoo/api/web/__init__.py
@@ -4,10 +4,9 @@
 from flask import Flask
 
 # Define the global URL prefix
-from pattoo_shared.constants import PATTOO_API_WEB_PREFIX
+from pattoo_shared.constants import PATTOO_API_SITE_PREFIX
 
 # Import PATTOO_API_WEB Blueprints
-from pattoo.constants import PATTOO_API_WEB_REST_PREFIX
 from pattoo.db import POOL
 from pattoo.api.web.graphql import GRAPHQL
 from pattoo.api.web.data import REST_API_DATA
@@ -15,12 +14,15 @@ from pattoo.api.web.datapoint import REST_API_DATAPOINT
 from pattoo.api.web.datasource import REST_API_DATASOURCE
 from pattoo.api.web.agent import REST_API_AGENT
 
+# Setup REST URI prefix
+PATTOO_API_WEB_REST_PREFIX = '{}/rest'.format(PATTOO_API_SITE_PREFIX)
+
 # Setup flask
 PATTOO_API_WEB = Flask(__name__)
 
 # Register Blueprints
 PATTOO_API_WEB.register_blueprint(
-    GRAPHQL, url_prefix=PATTOO_API_WEB_PREFIX)
+    GRAPHQL, url_prefix=PATTOO_API_SITE_PREFIX)
 PATTOO_API_WEB.register_blueprint(
     REST_API_DATA, url_prefix='{}/data'.format(PATTOO_API_WEB_REST_PREFIX))
 PATTOO_API_WEB.register_blueprint(
