8f2567e2b96e06b333cc85c2de1093469515c4d5
Peter Harrison <peter@colovore.com>
2019-12-28 06:41:34 -0800

    Added more logging for troubleshooting

--- bin/pattoo_ingesterd.py
+++ bin/pattoo_ingesterd.py
@@ -92,10 +92,12 @@ for possible causes.''')
             # Sleep. The duration could exceed the polling interval. Set sleep
             # time to the polling interval when this occurs.
             duration = time() - ts_start
-            if duration > interval:
+            if duration >= interval:
                 log_message = ('''Ingestion exceeded configured \
 "ingester_interval" parameter by {:6.2f}s.'''.format(duration - interval))
-            sleep_time = min(interval, abs(interval - duration))
+                sleep_time = 0
+            else:
+                sleep_time = abs(interval - duration)
             log_message = (
                 'Ingester sleeping for {:6.2f}s.'.format(sleep_time))
             log.log2info(20100, log_message)
