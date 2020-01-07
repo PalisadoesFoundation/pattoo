f0e38d0f77712ba3f0bf4a214d845b30347c5376
Peter Harrison <blackriver@simiya.com>
2020-01-06 21:17:56 -0800

    Fixes #191

--- pattoo/ingest/get.py
+++ pattoo/ingest/get.py
@@ -47,6 +47,9 @@ def key_value_pairs(pattoo_db_records):
         pattoo_db_records = [pattoo_db_records]
 
     for pattoo_db_record in pattoo_db_records:
+        # Get the agent program name
+        pap = pattoo_db_record.pattoo_agent_program
+
         # Iterate over NamedTuple
         for key, value in sorted(pattoo_db_record._asdict().items()):
 
@@ -57,9 +60,25 @@ def key_value_pairs(pattoo_db_records):
 
             if key == 'pattoo_metadata':
                 # Process the metadata key-values
-                rows.extend(value)
+                rows.extend([(make_key(pap, __k), __v) for __k, __v in value])
             else:
-                # Process other key-values
-                rows.append((str(key), str(value)))
+                # Process pattoo_key's key
+                rows.append((str(key), make_key(pap, value)))
 
     return sorted(rows)
+
+
+def make_key(pattoo_agent_program, key):
+    """Prepend the Agent program name to the key for uniqueness.
+
+    Args:
+        pattoo_agent_program: Program name
+        key: Key
+
+    Returns:
+        result: Result
+
+    """
+    # Return
+    result = '{}_{}'.format(pattoo_agent_program, key)
+    return result
