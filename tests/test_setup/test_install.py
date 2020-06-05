1e8b8be273deb81b1f815b827c253058ea5a8029
Jason Gayle <jasonblue1626@gmail.com>
2020-06-05 13:56:36 -0500

    Testing check_config and check_database unittests

--- tests/test_setup/test_install.py
+++ tests/test_setup/test_install.py
@@ -1,4 +1,4 @@
-
+from tests.libraries.configuration import UnittestConfig
 import os
 import getpass
 import unittest
@@ -18,37 +18,37 @@ else:
     print('''This script is not installed in the "{0}" directory. Please fix.\
 '''.format(_EXPECTED))
     sys.exit(2)
-from setup.installation_lib.install import _log, check_config
-from setup.installation_lib.install import check_pip3, check_database
-from tests.libraries.configuration import UnittestConfig
+
+from setup.installation_lib.install import check_pip3, check_database, check_config
+from setup.installation_lib.install import _log
 #from setup.install import _log
 
 
 class Test_Install(unittest.TestCase):
     """Checks all functions for the Pattoo install script."""
-    default_config = {
-        'pattoo_db': {
-            'db_pool_size': 10,
-            'db_max_overflow': 20,
-            'db_hostname': 'localhost',
-            'db_username': 'pattoo',
-            'db_password': 'password',
-            'db_name': 'pattoo'
-        },
-        'pattoo_api_agentd': {
-            'ip_listen_address': '0.0.0.0',
-            'ip_bind_port': 20201,
-        },
-        'pattoo_apid': {
-            'ip_listen_address': '0.0.0.0',
-            'ip_bind_port': 20202,
-        },
-        'pattoo_ingesterd': {
-            'ingester_interval': 3600,
-            'batch_size': 500
+     default_config = {
+            'pattoo': {
+                'language': 'en',
+                'log_directory': (
+                    '{1}{0}pattoo{0}log'.format(os.sep, opt_directory)),
+                'log_level': 'debug',
+                'cache_directory': (
+                    '{1}{0}pattoo{0}cache'.format(os.sep, opt_directory)),
+                'daemon_directory': (
+                    '{1}{0}pattoo{0}daemon'.format(os.sep, opt_directory)),
+                'system_daemon_directory': ('''\
+                    /var/run/pattoo''' if getpass.getuser() == 'root' else (
+                    '{1}{0}pattoo{0}daemon'.format(os.sep, run_dir)))
+            },
+            'pattoo_agent_api': {
+                'ip_address': '127.0.0.1',
+                'ip_bind_port': 20201
+            },
+            'pattoo_web_api': {
+                'ip_address': '127.0.0.1',
+                'ip_bind_port': 20202,
+            }
         }
-    }
-
     def test__init__(self):
         """Unnittest to test the __init__ function."""
         pass
@@ -57,6 +57,17 @@ class Test_Install(unittest.TestCase):
         """Unittest to test the install_missing function."""
         pass
 
+    def test_check_database(self):
+        """Unittest to test the check_database function."""
+        expected = True
+        result = check_database()
+        self.assertEqual(result, expected)
+
+    def test_check_config(self):
+        """Unittest to test the check_config function."""
+        expected = True
+        result = check_config()
+        self.assertEqual(result, expected)
 
     def test_check_pip3(self):
         """Unittest to test the check_pip3 function."""
@@ -70,11 +81,10 @@ class Test_Install(unittest.TestCase):
             _log("Test Error Message")
         self.assertEqual(cm.exception.code, 3)
 
-
 if __name__ == '__main__':
     # Make sure the environment is OK to run unittests
     print(os.environ['PATTOO_CONFIGDIR'])
-    UnittestConfig().create()
+    #UnittestConfig().create()
 
     # Do the unit test
-    unittest.main()
+    #unittest.main()
