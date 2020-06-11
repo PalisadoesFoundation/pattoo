09f21db2167ea7d478c0c3d13b953bf89ac08e13
Jason Gayle <jasonblue1626@gmail.com>
2020-06-11 02:13:07 -0500

    Venv test for travis

--- setup/installation_lib/make_venv.py
+++ setup/installation_lib/make_venv.py
@@ -2,7 +2,7 @@
 
 import os 
 import sys
-
+import getpass
 EXEC_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
 ROOT_DIR = os.path.abspath(os.path.join(EXEC_DIR, os.pardir))
 _EXPECTED = '{0}pattoo{0}setup{0}installation_lib'.format(os.sep)
@@ -27,13 +27,16 @@ def make_venv(venv_name):
     Returns:
         None
     """
-    venv_dir = os.path.join(os.path.expanduser('~'), venv_name)
-    venv_activate = '{0}{1}bin{1}activate'.format(venv_dir, os.sep)
-    print('??: Create virtual environment')
-    _run_script('python3 -m venv {0}'.format(venv_dir))
-    print('OK: Virtual environment created')
-    print('??: Activate virtual environment')
-    os.system('/bin/bash --rcfile {}'.format(venv_activate))
+    if getpass.getuser() == 'travis':
+        pass
+    else:
+        venv_dir = os.path.join(os.path.expanduser('~'), venv_name)
+        venv_activate = '{0}{1}bin{1}activate'.format(venv_dir, os.sep)
+        print('??: Create virtual environment')
+        _run_script('python3 -m venv {0}'.format(venv_dir))
+        print('OK: Virtual environment created')
+        print('??: Activate virtual environment')
+        os.system('/bin/bash --rcfile {}'.format(venv_activate))
 
 if __name__ == '__main__':
     make_venv('test-pattoo')
