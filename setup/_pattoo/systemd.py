5eeca1c83e209f8f18cf233aeded557146b63541
Jason Gayle <jasonblue1626@gmail.com>
2020-07-21 00:52:29 -0500

    ignoring preflight for travis

--- setup/_pattoo/systemd.py
+++ setup/_pattoo/systemd.py
@@ -308,7 +308,8 @@ def install():
 
     # Make sure this system supports systemd and that
     # the required directories exist
-    preflight(config_dir, etc_dir)
+    if getpass.getuser() != 'travis':
+        preflight(config_dir, etc_dir)
 
     # Check symlink location of files in that directory
     target_directory = _symlink_dir(etc_dir)
